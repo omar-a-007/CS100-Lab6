c3332b8de89a357e3c91ed96246a24b10eb78ec2
SevakOhanian7 <sevakohanian7@gmail.com>
2021-02-17 14:50:44 -0800

    adding CMake Files

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
 #include <iostream>
 #include "../header/rectangle.hpp"
-
+#include "../tests/test/cpp"
+#include "gtest/gtest.h"
 using namespace std;
 
-int main() 
+int main(int argc, char **argv) 
 {
-	Rectangle rect1, rect2;
-	rect1.set_width(3);
-	rect1.set_height(4);
-
-	rect2.set_width(4);
-	rect2.set_height(2);
-
-	cout << "Rectangle 1 area: " << rect1.area() << endl;
-	cout << "Rectangle 2 area: " << rect2.area() << endl;
-
-	return 0;
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
