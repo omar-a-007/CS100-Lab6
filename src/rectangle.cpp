b089fdaad8a73a0f0ab7de6f329a67541c80a4ec
SevakOhanian7 <sevakohanian7@gmail.com>
2021-02-17 14:27:23 -0800

    new success branch

---
+++ src/rectangle.cpp
@@ -0,0 +1,17 @@
+#include "../header/rectangle.hpp"
+
+void Rectangle::set_width(int w) {
+	this->width = w;
+}
+
+void Rectangle::set_height(int h) {
+	this->height = h;
+}
+
+int Rectangle::area() {
+	return this->width * this->height;
+}
+
+int Rectangle::perimeter(){
+	return (this->width * 2) + (this->height * 2);
+}
