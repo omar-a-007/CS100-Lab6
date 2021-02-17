69f90ed77e96fd1f26c4e6dd1adbe8952a386b78
SevakOhanian7 <sevakohanian7@gmail.com>
2021-02-17 15:27:37 -0800

    Added all test files

--- tests/rectangle_test.hpp
+++ tests/rectangle_test.hpp
@@ -3,6 +3,26 @@ ifndef __RECTANGLE_TESTS__
 #include "../header/rectangle.hpp"
 
 #include "gtest/gtest.h"}
+TEST(Constructor_Test, defaultRectangle)
+{
+        Rectangle rectangle = new Rectangle();
+        EXPECT_EQ(rectangle->getWidth(), 0);
+        EXPECT_EQ(rectangle->getHeight(), 0);
+}
+
+TEST(Constructor_Test, basicRectangle)
+{
+        Rectangle rectangle = new Rectangle(4, 5);
+        EXPECT_EQ(rectangle->getWidth(), 4);
+        EXPECT_EQ(rectangle->getHeight(), 5);
+}
+
+TEST(Constructor_Test, negativeRectangle)
+{
+        Rectangle rectangle = new Rectangle(-4,-5);
+        EXPECT_EQ(rectangle->getWidth(), -4);
+        EXPECT_EQ(rectangle->getHeight(), -5);
+}
 
 TEST(Perimeter_Test, regularPerimeter) {
 	Rectangle rectangle = new Rectangle(5, 6);
@@ -33,6 +53,43 @@ TEST(Perimeter_Test, doubleZero) {
         Rectangle rectangle = new Rectangle(0, 0);
         EXPECT_EQ(rectangle->perimeter(), 0);
 }
+
+TEST(Area_Test, noArea)
+{
+        Rectangle rectangle = new Rectangle();
+        EXPECT_EQ(rectangle->area(), 0);
+}
+
+TEST(Area_Test, singleZero)
+{
+        Rectangle rectangle = new Rectangle(4,0);
+        EXPECT_EQ(rectangle->area(), 0);
+}
+
+TEST(Area_Test, simpleArea)
+{
+        Rectangle rectangle = new Rectangle(4,5);
+        EXPECT_EQ(rectangle->area(), 20);
+}
+
+TEST(Area_Test, massiveArea)
+{
+        Rectangle rectangle = new Rectangle(5566432, 10);
+        EXPECT_EQ(rectangle->area(), 55664320);
+}
+
+TEST(Area_Test, oneNegative)
+{
+        Rectangle rectangle = new Rectangle(-4,5);
+        EXPECT_EQ(rectangle->area(), -20);
+}
+
+TEST(Area_Test, doubleNegative)
+{
+        Rectangle rectangle = new Rectangle(-4,-5);
+        EXPECT_EQ(rectangle->area(), 20);
+}
+
 
 
 #endif //__RECTANGLE_TESTS__
